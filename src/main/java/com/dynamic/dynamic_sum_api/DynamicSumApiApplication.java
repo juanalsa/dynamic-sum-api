f9be28e0521dac9b8adacfff4351c3f525fc8edc
${'Julian Alvarado'} <juanalsa@gmail.com>
2025-04-12 18:45:15 -0500

    Base project

---
+++ src/main/java/com/dynamic/dynamic_sum_api/DynamicSumApiApplication.java
@@ -0,0 +1,13 @@
+package com.dynamic.dynamic_sum_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynamicSumApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DynamicSumApiApplication.class, args);
+	}
+
+}
